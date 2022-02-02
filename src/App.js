dd4329c1fd2e11816bfe18789328c02f8b7c92e7
tavolafourcade <tavolafourcade@gmail.com>
2022-02-02 14:01:23 -0500

    [ ADD ] Basic implementation of useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState } from 'react'
 
-function App() {
+const Header = () => {
+  const styles = {
+    background: 'linear-gradient(20deg, #6813cb, #2575fc)',
+    textAlign: 'center',
+    borderRadius: '0.2em',
+    color: '#FFF',
+    padding: '0.3em',
+    margin: '0.3em',
+    fontSize: '14px'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <header style={styles}>
+      <h1>
+        Hook useState
+        <span
+          role='img'
+          aria-label='hook emoji'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          ⚓
+        </span> 
+      </h1>
+    </header>
+  )
 }
 
-export default App;
+const App = () => {
+  const [ clicks, setClicks ] = useState(0)
+
+  const addClicks = () => {
+    setClicks(clicks + 1)
+  }
+
+  return (
+    <div>
+      <Header />
+      <button onClick={addClicks}>
+        Clicks ({ clicks })
+      </button>
+    </div>
+  )
+}
+
+// class App extends Component {
+//   state = {
+//     clicks: 0
+//   }
+
+//   addClicks = () => {
+//     this.setState(state => ({
+//       clicks: state.clicks + 1
+//     }))
+//   }
+
+//   render () {
+//     const { clicks } = this.state
+//     return (
+//       <div>
+//         <Header />
+//         <button onClick={this.addClicks}>
+//           Clicks ({ clicks })
+//         </button>
+//       </div>
+//     )
+//   }
+// }
+
+export default App
