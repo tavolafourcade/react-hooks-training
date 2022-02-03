011d701632a1c885c65d31872da1ad234246037f
tavolafourcade <tavolafourcade@gmail.com>
2022-02-03 17:53:12 -0500

    [ ADD ] Input search request of product name to the API

--- src/App.js
+++ src/App.js
@@ -1,60 +1,36 @@
-import React, { createContext, useState, useContext } from 'react'
-
-// Consumir context de forma tradiciona
-// const Nieto = () => (
-//   <MyContext.Consumer>
-//     {
-//       (context) => (
-//         <div>
-//           <p>Nieto {context.num}</p>
-//           <button onClick={context.addNum}>
-//             Nieto Dispatcher
-//           </button>
-//         </div>
-//       )
-//     }
-//   </MyContext.Consumer>
-// )
-
-// { Provider, Consumer }
-const MyContext = createContext()
-
-const Nieto = () => {
-  const {num,addNum} = useContext(MyContext)
-  return (
-    <div>
-      <p>
-        Nieto {num}
-      </p>
-      <button onClick={addNum}>
-        Nieto Dispatcher
-      </button>
-    </div>
-  )
-}
-
-const Hijo = () => (
-  <div>
-    <p>Hijo</p>
-    <Nieto />
-  </div>
-)
+import React, { useState, useEffect, useRef } from 'react'
 
 const App = () => {
-  const [ num, setNum ] = useState(0)
-  const addNum = () => setNum(num+1)
+  const [ name, setName ] = useState('')
+  const [ products, setProducts ] = useState([])
+
+  const entrada = useRef()
+
+  useEffect(() => {
+    // Products request
+    fetch('https://universidad-react-api-test.luxfenix.vercel.app/products?name=' + name)
+    .then(res => res.json())
+    .then(data => setProducts(data.products))
+  },[name])
+
+  const handleInput = (e) => {
+    setName(e.target.value)
+  }
   return (
-    <MyContext.Provider value={{
-      num,
-      addNum
-    }}>
       <div>
-        <button onClick={addNum}>
-          App ( {num})
-        </button>
-        <Hijo/>
+        <input 
+          type='text'
+          onChange={handleInput}
+          ref={entrada}
+          />
+          <ul>
+            {products.map( product => (
+              <li key={product.id}>
+                {product.name}
+              </li>
+            ))}
+          </ul>
       </div>
-    </MyContext.Provider>
   )
 }
 
