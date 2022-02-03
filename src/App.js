b6286231f817c7ac8473be8db1740451900722fe
tavolafourcade <tavolafourcade@gmail.com>
2022-02-03 13:58:04 -0500

    [ ADD ] Fetch API data validation

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React, { Component, useEffect, useState } from 'react'
 
 const App = () => {
   const [ users, setUsers ] = useState([])
+  const [ isFetching, setIsFetching ] = useState(true)
+
 
   useEffect(()=> {
     // We transform the API response to JSON
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
-    .then(users => setUsers(users))
+    .then(users => {
+      setUsers(users)
+      setIsFetching(false)
+    })
   },[])
-
 
   return (
     <div>
+      {isFetching && <h1>Cargando...</h1>}
       <ul>
         { users.map(user => (
           <li key={user.id}>
