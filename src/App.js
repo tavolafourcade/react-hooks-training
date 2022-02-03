a3066d3e123865e65f052fab034261961fb34118
tavolafourcade <tavolafourcade@gmail.com>
2022-02-03 13:17:19 -0500

    [ ADD ] Fetching API data and return the names

--- src/App.js
+++ src/App.js
@@ -1,60 +1,25 @@
 import React, { Component, useEffect, useState } from 'react'
 
-const Header = () => {
-  const styles = {
-    background: 'linear-gradient(20deg, #6813cb, #2575fc)',
-    textAlign: 'center',
-    borderRadius: '0.2em',
-    color: '#FFF',
-    padding: '0.3em',
-    margin: '0.3em',
-    fontSize: '14px'
-  }
-
-  return (
-    <header style={styles}>
-      <h1>
-        Hook useState
-        <span
-          role='img'
-          aria-label='hook emoji'
-        >
-          ⚓
-        </span> 
-      </h1>
-    </header>
-  )
-}
-
 const App = () => {
-  const [ num, setNum ] = useState(0)
-  const [ emoji, setEmoji ] = useState('🤓')
+  const [ users, setUsers ] = useState([])
 
   useEffect(()=> {
-    alert('useEffect')
-  },[num])
+    // We transform the API response to JSON
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(users => setUsers(users))
+  },[])
 
-  const addClicks = () => {
-    setNum(num + 1)
-  }
-
-  const toggleEmoji = () => {
-    const nextEmoji = emoji === '🤓' ? '🥳' : '🤓'
-    setEmoji(nextEmoji)
-  }
 
   return (
     <div>
-      <Header/>
-      <button onClick={addClicks}>
-          ADD ({ num })
-      </button>
-      <button onClick={toggleEmoji}>
-        Alternar Emoji
-      </button>
-      <h1>
-        { emoji }
-      </h1>
+      <ul>
+        { users.map(user => (
+          <li key={user.id}>
+            {user.name}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
