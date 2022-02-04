867524f09f7b48f5d2f3bfbd23ca0582c37a554d
tavolafourcade <tavolafourcade@gmail.com>
2022-02-04 13:35:48 -0500

    [ ADD ] Implementation of useCallback() with dependencies

--- src/App.js
+++ src/App.js
@@ -20,18 +20,27 @@ const Button = memo(({callback, children}) => {
 
 const App = () => {
   const [ a, setA ] = useState(0)
+  const [ b, setB ] = useState(0)
+
 
   const incrementA = useCallback(() => {
     setA(a=> a + 1)
   }, [])
+
+  const incrementB = useCallback(() => {
+    setB(b=> b + a)
+  }, [a])
 
   return (
       <div>
         <Button callback={incrementA}>
           Increment A
         </Button>
+        <Button callback={incrementB}>
+          Increment B
+        </Button>
         <h1>
-          a:  { a }
+          a:  { a } b: { b }
         </h1>
       </div>
   )
