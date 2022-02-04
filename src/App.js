5b7087b440c9c2483a3c502d51dbe3e62b165713
tavolafourcade <tavolafourcade@gmail.com>
2022-02-04 12:40:39 -0500

    [ ADD ] Implementation of React.memo() with manual diferentiation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, memo } from 'react'
 
-const Counter = ({ count }) => {
+// React.memo()
+const Counter = memo(({ count }) => {
   console.log('%cRender <Counter />', 'color:blue');
   return (
     <h1>
       { count }
     </h1>
   )
-}
+})
 
-const Title = ({ text }) => {
+const Title = memo(({ text }) => {
   console.log('%cRender <Title />', 'color:orangered');
   return (
     <h1>
       { text }
     </h1>
   )
-}
+})
+
+const TitleNested = memo(({ info }) => {
+  console.log('%cRender <TitleNested />', 'color:purple');
+  return (
+    <h1>
+      { info.text }
+    </h1>
+  )
+},
+  // Algoritmo de diferenciación
+  (prevProps, nextProps)=> {
+    //console.log(prevProps, nextProps);
+    // Si retorna true no se va a renderizar: cuando text es igual
+    // Si retorna false se va a renderizar: cuando text cambió
+    return prevProps.info.text === nextProps.info.text
+  }
+)
 
 const App = () => {
   const [ title, setTitle ] = useState('')
@@ -41,6 +59,9 @@ const App = () => {
           </button>
           <Counter count={count}/>
           <Title text={title} />
+          <TitleNested info={{
+            text: title
+          }} />
       </div>
   )
 }
